4bab02a7b1c9340d5a817868c1172f90fc843c93
Rui Mestre <rui.mestre@deimos.com.pt>
2017-01-16 11:42:27 +0000

    FizzBuzz: Step 1

--- src/com/ee/fizzbuzz/FizzBuzz.java
+++ src/com/ee/fizzbuzz/FizzBuzz.java
@@ -8,11 +8,23 @@ public class FizzBuzz {
 	public static String FIZZBUZZ = "fizzbuzz";
 
 	public static String fizzBuzz(int i) {
-		return "";
+		if(i%15==0) {
+			return FIZZBUZZ;
+		} else if(i%3==0) {
+			return FIZZ;
+		} else if(i%5==0) {
+			return BUZZ;
+		} else {
+			return String.valueOf(i);
+		}
 	}
 
 	public static String continuousFizzBuzz(int min, int max) {
-		return "";
+		StringBuilder result = new StringBuilder();
+		for(int i = min; i<=max; ++i) {
+			result.append(fizzBuzz(i)).append(" ");
+		}
+		return result.toString();
 	}
 
 	public static void main(String[] args) {
